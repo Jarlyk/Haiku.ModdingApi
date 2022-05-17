41d00b99d6c098ff935649cc73b390c7eb2c93bd
schyvun <julisullek@gmx.de>
2022-05-18 00:19:10 +0200

    Added PluginOverlay, added Resolution Settings, removed Steam Name, added CanvasUtil

--- Haiku.CoreModdingApi/MapiPlugin.cs
+++ Haiku.CoreModdingApi/MapiPlugin.cs
@@ -1,13 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using BepInEx;
 using UnityEngine;
+using BepInEx.Bootstrap;
+using UnityEngine.UI;
 
 namespace Haiku.CoreModdingApi
 {
     [BepInPlugin("haiku.mapi", "Haiku Core Modding API", "1.0.0.0")]
     public sealed class MapiPlugin : BaseUnityPlugin
     {
+
+        void Start()
+        {
+            List<string> pluginsLoaded = new List<string>();
+
+            // Remove Steam Name Display and get the font
+            GameObject SteamNameDisplay = GameObject.Find("WelcomeText");
+            Font font = SteamNameDisplay.GetComponent<Text>().font;
+            Destroy(SteamNameDisplay);
+
+            // Disabling Achievements
+            On.AchievementManager.SetAchievement += onAchievement;
+
+            // MApi settings
+            Settings.initSettings(Config);
+
+            // MApi overlay
+            Logger.LogInfo("Trying to find plugins");
+            foreach (var plugin in Chainloader.PluginInfos)
+            {
+                var metadata = plugin.Value.Metadata;
+                Logger.LogInfo("Plugin found: " + metadata.Name);
+                if (metadata.Name.Equals("Haiku Core Modding API") && pluginsLoaded.Count > 0)
+                {
+                    // Put MApi on top because why not
+                    pluginsLoaded.Add(pluginsLoaded[0]);
+                    pluginsLoaded[0] = metadata.Name.ToString() + ": " + metadata.Version.ToString();
+                }
+                else
+                {
+                    pluginsLoaded.Add(metadata.Name.ToString() + ": " + metadata.Version.ToString());
+                }
+            }
+            if (pluginsLoaded.Count == 0)
+            {
+                Logger.LogInfo("Couldn't find plugins");
+            }
+
+            GameObject MApiHeader = new GameObject();
+            MApiHeader.name = "MApi Header";
+            Canvas MApiCanvas = MApiHeader.AddComponent<Canvas>();
+            MApiCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+            CanvasScaler MApiCanvasScaler = MApiHeader.AddComponent<CanvasScaler>();
+            MApiCanvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            MApiCanvasScaler.referencePixelsPerUnit = 16f;
+            MApiCanvasScaler.referenceResolution = new Vector2(384f, 216f);
+            MApiCanvas.sortingOrder = 1;
+
+
+            CanvasAspectScaler MApiCanvasAspectScaler = MApiHeader.AddComponent<CanvasAspectScaler>();
+            MApiHeader.AddComponent<GraphicRaycaster>();
+
+            DontDestroyOnLoad(MApiHeader);
+
+            GameObject MapiPlugins = new GameObject();
+            MapiPlugins.transform.parent = MApiHeader.transform;
+            MapiPlugins.transform.localPosition = new Vector3(10, 103, 0);
+            MapiPlugins.name = "MApi Plugins";
+            for (int i = 0; i < pluginsLoaded.Count; i++)
+            {
+                GameObject textObject = new GameObject();
+                textObject.transform.parent = MapiPlugins.transform;
+                textObject.name = $"{pluginsLoaded[i]}";
+                Text text = textObject.AddComponent<Text>();
+                //text.font = Resources.GetBuiltinResource(typeof(Font), "Jaldi.ttf") as Font;
+                text.font = font;
+                text.fontStyle = FontStyle.Bold;
+                text.text = pluginsLoaded[i];
+                text.fontSize = 4;
+
+                // Text position
+                RectTransform rectTransform = text.GetComponent<RectTransform>();
+                rectTransform.localPosition = new Vector3(0, 0 - i * 5, 0);
+                rectTransform.sizeDelta = new Vector2(400, 10);
+            }
+        }
+
+        #region Hooks
+        private void onAchievement(On.AchievementManager.orig_SetAchievement orig, AchievementManager self, string achievementName)
+        {
+            Debug.LogWarning("Tried to get an Achievement while MApi is enabled, passing instead");
+        }
+        #endregion
     }
 }
---
+++ Haiku.CoreModdingApi/Settings.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.ComponentModel;
+
+namespace Haiku.CoreModdingApi
+{
+    internal class Settings
+    {
+        public static void initSettings(BepInEx.Configuration.ConfigFile config)
+        {
+            config.Bind<possibleResolutions>("MApi", "Resolution", possibleResolutions.Res1920x1080);
+            if (config.TryGetEntry<possibleResolutions>(new BepInEx.Configuration.ConfigDefinition("MApi", "Resolution"), out BepInEx.Configuration.ConfigEntry<possibleResolutions> entry))
+            {
+                changeResolution(GetRes((possibleResolutions)entry.BoxedValue));
+            }
+            config.SaveOnConfigSet = true;
+            config.SettingChanged += Config_SettingChanged;
+        }
+        #region ResolutionConfigs
+        private enum possibleResolutions
+        {
+            [Description("256x144")]
+            Res256x144,
+            [Description("320x180")]
+            Res320x180,
+            [Description("426x240")]
+            Res426x240,
+            [Description("640x360")]
+            Res640x360,
+            [Description("848x480")]
+            Res848x480,
+            [Description("960x540")]
+            Res960x540,
+            [Description("1280x720")]
+            Res1280x720,
+            [Description("1600x900")]
+            Res1600x900,
+            [Description("1920x1080")]
+            Res1920x1080,
+            [Description("2560x1440")]
+            Res2560x1440,
+            [Description("3200x1800")]
+            Res3200x1800,
+            [Description("3840x2160")]
+            Res3840x2160
+        }
+        private static readonly Vector2[] resolutions = new Vector2[]
+        {
+            new Vector2(256f,144f),
+            new Vector2(320f,180f),
+            new Vector2(426f,240f),
+            new Vector2(640f,360f),
+            new Vector2(848f,480f),
+            new Vector2(960f,540f),
+            new Vector2(1280f,720f),
+            new Vector2(1600f,900f),
+            new Vector2(1920f,1080f),
+            new Vector2(2560f,1440f),
+            new Vector2(3200f,1800f),
+            new Vector2(3840f,2160f),
+        };
+
+        private static Vector2 GetRes(possibleResolutions res)
+        {
+            return resolutions[(int)res];
+        }
+
+        private static void Config_SettingChanged(object sender, BepInEx.Configuration.SettingChangedEventArgs e)
+        {
+            if (e.ChangedSetting.Definition == new BepInEx.Configuration.ConfigDefinition("MApi", "Resolution"))
+            {
+                Vector2 res = GetRes((possibleResolutions)e.ChangedSetting.BoxedValue);
+                changeResolution(res);
+            }
+        }
+
+        private static void changeResolution(Vector2 res)
+        {
+            Screen.SetResolution((int)res.x, (int)res.y, Screen.fullScreen);
+        }
+        #endregion
+    }
+}
